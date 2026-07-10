8a6f1fc2f4d6adc3700a54c423b392fe3f6ea00f
Thomas Carriere <thomas7carriere@gmail.com>
2026-07-09 20:30:22 -0400

    Added tests for NIC and Ubuntu Version check

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-azure-subscription-id"
+var subscriptionID string = "135c0fd2-233e-4da4-bc1d-eac95325b187"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-id>",
+			"labelPrefix": "deha0036",
 		},
 	}
 
@@ -27,10 +27,21 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Run `terraform output` to get the value of output variable
+	// Run `terraform output` to get the value of output variables
 	vmName := terraform.Output(t, terraformOptions, "vm_name")
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
+	nicName := terraform.Output(t, terraformOptions, "nic_name")
 
 	// Confirm VM exists
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+
+	// Confirm NIC exists and is connected to the VM
+	actualNics := azure.GetVirtualMachineNics(t, vmName, resourceGroupName, subscriptionID)
+	assert.Contains(t, actualNics, nicName)
+
+	// Confirm the VM is running the correct Ubuntu version
+	actualImage := azure.GetVirtualMachineImage(t, vmName, resourceGroupName, subscriptionID)
+	assert.Equal(t, "Canonical", actualImage.Publisher)
+	assert.Equal(t, "0001-com-ubuntu-server-jammy", actualImage.Offer)
+	assert.Equal(t, "22_04-lts-gen2", actualImage.SKU)
 }
